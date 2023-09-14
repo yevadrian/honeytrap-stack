97a88dd26396d37dcef4b6b638576c0c4032527d
Yevadrian Andreas <110159876+yevadrian@users.noreply.github.com>
2023-09-14 23:29:18 +0700

    Update connectors.sh

--- connectors.sh
+++ connectors.sh
@@ -1,3 +1,6 @@
+# Install Additional Tools
+apt install jq -y
+
 # Put Source Connector to Kafka Connect
 curl -sX PUT "http://localhost:8083/connectors/source-filepulse/config" \
 -d @connectors/source-filepulse.json -H "Content-Type: application/json" | jq
@@ -5,15 +8,3 @@ curl -sX PUT "http://localhost:8083/connectors/source-filepulse/config" \
 # Put Sink Connector to Kafka Connect
 curl -sX PUT "http://localhost:8083/connectors/sink-mongodb/config" \
 -d @connectors/sink-mongodb.json -H "Content-Type: application/json" | jq
-
-# Check Source Connector Status from Kafka Connect
-# curl -sX GET "http://localhost:8083/connectors/source-filepulse/status" | jq
-
-# Check Sink Connector Status from Kafka Connect
-# curl -sX GET "http://localhost:8083/connectors/sink-mongodb/status" | jq
-
-# Delete Source Connector Status from Kafka Connect
-# curl -sX DELETE "http://localhost:8083/connectors/source-filepulse"
-
-# Delete Source Connector Status from Kafka Connect
-# curl -sX DELETE "http://localhost:8083/connectors/sink-mongodb"
